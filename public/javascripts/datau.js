78a3ce7e872b07f382ae729441f4af324263a64d
Dong Liu <liud@frib.msu.edu>
2014-12-11 10:55:17 -0500

    add jsonpath, part of ccf column tested

--- public/javascripts/datau.js
+++ public/javascripts/datau.js
@@ -1,29 +1,54 @@
-/*global moment: false, Dygraph: false*/
+/*global moment: false, Dygraph: false, jsonPath*/
 function padding(s) {
   return '0' + s;
 }
 
 // the template to etl the json
 var template = {
-  ccf_experiment_number : {
-    e : '$[0].experiment.number',
-    t : padding,
-    l : '#ccf_experiment_number'
+  ccf_experiment_number: {
+    e: '$[0].experiment.number',
+    t: padding,
+    l: '#ccf_experiment_number'
   },
-    ccf_experiment_spokesperson : {
-    e : '$[0].experiment.spokesperson',
-    l : '#ccf_experiment_spokesperson'
+  ccf_experiment_spokesperson: {
+    e: '$[0].experiment.spokesperson',
+    l: '#ccf_experiment_spokesperson'
   },
-    ccf_experiment_title : {
-    e : '$[0].experiment.title',
-    l : '#ccf_experiment_title'
+  ccf_experiment_title: {
+    e: '$[0].experiment.title',
+    l: '#ccf_experiment_title'
   },
-    ccf_experiment_a1900Contact : {
-    e : '$[0].experiment.a1900Contact',
-    l : '#ccf_experiment_a1900Contact'
+  ccf_experiment_a1900Contact: {
+    e: '$[0].experiment.a1900Contact',
+    l: '#ccf_experiment_a1900Contact'
   }
-
 };
+
+function jsonETL(json, template) {
+  var prop, value;
+  for (prop in template) {
+    if (template.hasOwnProperty(prop)) {
+      value = jsonPath.eval(json, template[prop].e);
+      if (value.length === 1) {
+        if (typeof template[prop].t === 'function') {
+          $(template[prop].l).text(template[prop].t(value[0]));
+          // output[prop] = template[prop].t(value[0]);
+        } else {
+          $(template[prop].l).text(value[0]);
+          // output[prop] = value[0];
+        }
+      } else {
+        if (template[prop].hasOwnProperty('defaultValue')) {
+          $(template[prop].l).text(template[prop].defaultValue);
+          // output[prop] = template[prop].defaultValue;
+        } else {
+          $(template[prop].l).text('');
+          // output[prop] = null;
+        }
+      }
+    }
+  }
+}
 
 function updateClock() {
   var now = moment();
@@ -62,6 +87,15 @@ $(function () {
       colors: ['#0033CC'],
       height: 150
     });
+  }).fail(function (jqXHR, status, error) {
+    //do something;
+  });
+
+  $.ajax({
+    url: '/facilities/summary',
+    dataType: 'json'
+  }).done(function (json) {
+    jsonETL(json, template);
   }).fail(function (jqXHR, status, error) {
     //do something;
   });
